15c15a1b37e54e3e01642677e4d22e36ec2984ae
purveyortickets <266715712+purveyortickets@users.noreply.github.com>
2026-03-30 21:08:24 +0800

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,8 +1,8 @@
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import Services from "@/components/Services";
-import UpcomingEvents from "@/components/OngoingEvents";
 import Testimonials from "@/components/Testimonials";
+import TwitterFeed from "@/components/TwitterFeed";
 import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
 
@@ -12,8 +12,8 @@ const Index = () => {
       <Header />
       <Hero />
       <Services />
-      <UpcomingEvents />
       <Testimonials />
+      <TwitterFeed />
       <Contact />
       <Footer />
     </div>
